433f7b7445033055494e5c1057b604c938b89617
ANANTH-SWAMY <75056416+ANANTH-SWAMY@users.noreply.github.com>
2022-06-30 18:30:51 +0530

    Add files via upload

--- src/wfetch.bat
+++ src/wfetch.bat
@@ -1,1 +1,17 @@
+@echo off
+
+for /f "tokens=3,4,5,6 usebackq" %%i in (`systeminfo ^| findstr /C:"System Model"`) do (set name=%%i %%j %%k %%l)
+
+for /f "usebackq tokens=4,5,6" %%i in (`systeminfo ^| findstr /C:"OS Name"`) do (set osname=%%i %%j %%k)
+
+for /f "usebackq tokens=6" %%i in (`systeminfo ^| findstr /B /C:"OS Version"`) do (set osver=%%i)
+
+echo   [94m ___________
+echo   ^|     ^|     ^|  [92m %username%[0m@[92m%computername%[94m
+echo   ^|     ^|     ^|   
+echo   ^|_____^|_____^|  [96m os[0m          %osname%[94m
+echo   ^|     ^|     ^|  [96m host[0m        %name%[94m
+echo   ^|     ^|     ^|  [96m version[0m     %osver%[94m
+echo   ^|_____^|_____^| 
+echo [0m
 
